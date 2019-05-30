ee7046dc87f03ea97e1cf27e0835ea501b655a59
Carmine White <c.white@ahkclean.com>
2019-05-29 20:22:01 -0700

    finished initial CRUD routes for the models.  They all work

--- temp/index.js
+++ temp/index.js
@@ -35,3 +35,48 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
+
+
+
+////////////////////////////
+////MODULARIZATION WITH MODELS:
+// const Company = require("../models").Company;
+// const Customer = require("../models").Customer;
+// const Employee = require("../models").Employee;
+// const Timecard = require("../models").Timecard;
+const User = require("../models").User;
+// const Job = require("../models").Job;
+// const employees_jobs = require("../models").employees_jobs;
+
+
+
+////////////////  NOT-WORKING!!!!!!!!!!!!  /////////////////////////////
+//EXPORT OUR CONTROLLERS SO OUR ROUTES CAN ACCESS IT
+//==================   Need to modularize this more ==============================//
+module.exports = {
+    //User Routes
+
+    users_all: (req, res) => {
+        User.findAll({})
+        .then( data => res.send({success: true, payload: data}))
+        .catch( err => res.send({success: false, error: err}))
+        console.log(data)
+        },
+    }
+    
+
+
+/////////////////// WORKING!!!!!!!!!!!! /////////////////////////////
+module.exports = {
+  all: (req, res) => {
+      User.findAll({})
+      .then((foundUser) => {
+          res.send({'status':true,'data':foundUser});
+          console.log(foundUser)
+      })
+      .catch((err) => {
+          console.log("Error finding user", err)
+      })
+      },
+  }
